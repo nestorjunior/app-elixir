4c7c60ccd86b544a76c46094b4b1ee66a33fded1
nestorjunior <contato.nestor@gmail.com>
2019-08-30 09:50:53 -0300

    refatoring function

--- lib/app_elixir/cli/menu_choice.ex
+++ lib/app_elixir/cli/menu_choice.ex
@@ -1,19 +1,20 @@
 defmodule AppElixir.CLI.MenuChoice do
     alias Mix.Shell.IO, as: Shell
+    alias AppElixir.CLI.MenuItems
 
     def start do
         Shell.cmd("clear")
         Shell.info("Choose an option: ")
         
-        menu_items = AppElixir.CLI.MenuItems.all()
-        find_menu_item_by_index = &Enum.at(menu_items, &1)
+        menu_items = MenuItems.all()
+        find_menu_item_by_index = &Enum.at(menu_items, &1, :error)
 
         menu_items
         |> Enum.map(&(&1.label))
         |> display_options()
         |> generate_question()
         |> Shell.prompt
-        |> parser_answer()
+        |> parse_answer()
         |> find_menu_item_by_index.()
         |> confirm_menu_item()
     end
@@ -30,9 +31,18 @@ defmodule AppElixir.CLI.MenuChoice do
         "What is your option? [#{n}]\n"
     end 
 
-    defp parser_answer(answer) do
-        {n, _} = Integer.parse(answer)
-        n - 1
+    defp parse_answer(answer) do
+        case Integer.parse(answer) do
+            :error -> invalid_option()
+            {n, _} -> n - 1
+        end
+    end
+
+    defp invalid_option() do
+        Shell.cmd("clear")
+        Shell.error("Sorry: Invalid option")
+        Shell.prompt("Press enter to return!")
+        start()
     end
 
     defp confirm_menu_item(chosen_menu_item) do
@@ -40,7 +50,7 @@ defmodule AppElixir.CLI.MenuChoice do
         Shell.info("Your choice:  [#{chosen_menu_item.label}]")
 
         if Shell.yes?("Ok?") do
-            Shell.info("#{chosen_menu_item.label}")
+            Shell.info("... #{chosen_menu_item.label} ...")
         else 
             start()
         end
